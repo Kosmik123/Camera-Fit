a40bc72005e82f9e2e41c0bdcf6e0b282308f77c
Kosmik123 <malpa.w.klapkach@gmail.com>
2021-12-27 02:28:16 +0100

    Initial commit

---
+++ Assets/Scripts/Camera Fit/CameraMainArea.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+
+public class CameraMainArea : CameraFit
+{
+    public new Camera camera;
+
+    [SerializeField]
+    private Vector2 _mainAreaPosition;
+    public Vector2 Position
+    {
+        get { return _mainAreaPosition; }
+        set
+        {
+            _mainAreaPosition = value;
+            Resize();
+        }
+    }
+
+    [SerializeField]
+    private Vector2 _mainAreaSize = new Vector2(4,4);
+    public Vector2 Size
+    {
+        get { return _mainAreaSize; }
+        set
+        {
+            _mainAreaSize = value;
+            Resize();
+        }
+    }
+
+
+    [SerializeField] [Range(-1, 1)]
+    private float _horizontalShift, _verticalShift;
+    public Vector2 Shift
+    {
+        get { return new Vector2(_horizontalShift, _verticalShift); }
+        set
+        {
+            _horizontalShift = Mathf.Clamp(value.x, -1, 1);
+            _verticalShift = Mathf.Clamp(value.y, -1, 1);
+            Resize();
+        }
+    }
+
+    [SerializeField]
+    private float _zoom = 1;
+    public float Zoom { 
+        get { return _zoom; } 
+        set 
+        {
+            _zoom = value;
+            Resize();     
+        } 
+    }
+
+    private void Awake()
+    {
+        Resize();
+    }
+
+    protected override void Resize()
+    {
+        if (camera == null || !camera.orthographic)
+            return;
+
+        float cameraSize = _mainAreaSize.y / _zoom;
+        float camWidth = cameraSize * camera.aspect;
+        Vector3 newCamPos = camera.transform.localPosition;
+        newCamPos.x = _mainAreaPosition.x + (_mainAreaSize.x - camWidth) * _horizontalShift * 0.5f;
+        newCamPos.y = _mainAreaPosition.y;
+        if (camWidth < _mainAreaSize.x)
+        {
+            cameraSize = _mainAreaSize.x / (camera.aspect * _zoom);
+            newCamPos.y = _mainAreaPosition.y - (_mainAreaSize.y - cameraSize) * _verticalShift * 0.5f;
+            newCamPos.x = _mainAreaPosition.x;
+        }
+
+        ApplyChanges(newCamPos, cameraSize);
+    }
+
+    private void ApplyChanges(Vector3 position, float size)
+    {
+        camera.orthographicSize = 0.5f * size;
+        camera.transform.localPosition = position;
+    }
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        if (_mainAreaSize.x <= 0)
+            _mainAreaSize.x = 0.00001f;
+        if (_mainAreaSize.y <= 0)
+            _mainAreaSize.y = 0.00001f;
+        if (_zoom == 0)
+            _zoom = 0.0001f;
+
+        if (active)
+            Resize();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireCube(_mainAreaPosition, _mainAreaSize);
+
+        if (active)
+            Resize();
+    }
+
+#endif
+}
---
+++ Assets/Scripts/Camera Fit/CameraOrthographicSize.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class CameraFit : MonoBehaviour
+{
+    public bool active = true;
+    private float lastAspect;
+    protected Camera cam;
+
+    public event EventHandler OnCameraResize;
+
+    protected abstract void Resize();
+
+    protected void Refresh()
+    {
+        if (cam == null)
+            cam = GetComponent<Camera>();
+        if (Mathf.Abs(cam.aspect - lastAspect) > 0.01f)
+        {
+            Resize();
+            OnCameraResize?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
+
+
+public class CameraOrthographicSize : CameraFit
+{
+    [Range(0, 1)]
+    public float horizontalFit;
+    public float size;
+
+    private void Awake()
+    {
+        Resize();
+    }
+
+
+    protected override void Resize()
+    {
+        if (cam == null)
+            cam = GetComponent<Camera>();
+        if (!cam.orthographic)
+            return;
+
+        cam.orthographicSize = size * ((1 - horizontalFit) + horizontalFit / cam.aspect);
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        if (active)
+            Resize();
+    }
+
+    private void OnValidate()
+    {
+        if (active)
+            Resize();
+    }
+#endif
+}
